3d700f331960fe5cc5b380d1ae67df5462c3a10f
ankit <ankit.sharma21598@gmail.com>
2026-04-29 13:32:45 +0530

    Added migration setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,9 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+config({
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || "dev"}`),
+});
 
 const {
     PORT,
