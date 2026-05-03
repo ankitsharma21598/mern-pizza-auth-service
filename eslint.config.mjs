bcb0f24c4025fac07d1e56e2716ed78e886ebfca
ankit <ankit.sharma21598@gmail.com>
2026-05-03 21:00:56 +0530

    Add test coverage percentage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig(
             "scripts/**",
             "*.spec.ts",
             "tests/",
+            "coverage/**",
         ],
     },
     {
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,20 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         fileParallelism: false,
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "html", "json-summary"],
+            reportsDirectory: "./coverage",
+            include: ["src/**/*.ts"],
+            exclude: [
+                "src/**/*.spec.ts",
+                "src/**/*.test.ts",
+                "tests/",
+                "tests/**",
+                "node_modules/**",
+                "src/migration/**",
+                "src/config/data-source.ts",
+            ],
+        },
     },
 });
