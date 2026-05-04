eed46ce8817716515d6de60503fd46a87eaf3660
ankit <ankit.sharma21598@gmail.com>
2026-05-04 18:12:24 +0530

    updated migration import

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -4,10 +4,6 @@ import { Config } from "./index.js";
 import { User } from "../entity/User.js";
 import { RefreshToken } from "../entity/RefreshToken.js";
 import { Tenant } from "../entity/Tenant.js";
-import { Migration1777448881660 } from "../migration/1777448881660-migration.js";
-import { RenameTables1777450613716 } from "../migration/1777450613716-rename_tables.js";
-import { CreateTenantsTable1777526483055 } from "../migration/1777526483055-create_tenants_table.js";
-import { AddTenantIDForeignKey1777527055463 } from "../migration/1777527055463-add_tenantID_foreign_key.js";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -20,11 +16,6 @@ export const AppDataSource = new DataSource({
     synchronize: false,
     logging: false,
     entities: [User, RefreshToken, Tenant],
-    migrations: [
-        Migration1777448881660,
-        RenameTables1777450613716,
-        CreateTenantsTable1777526483055,
-        AddTenantIDForeignKey1777527055463,
-    ],
+    migrations: ["src/migration/*.{ts,js}"],
     subscribers: [],
 });
